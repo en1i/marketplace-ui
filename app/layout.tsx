acfe880199d94701ef34dff241620be1537ec09f
Alex <alex@lightbeam.ai>
2026-04-26 18:17:04 +0200

    add favicons. Update deployment docs

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,15 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Marketplace',
   description: 'Marketplace for all your needs',
+  icons: {
+    icon: [
+      { url: '/favicons/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+      { url: '/favicons/favicon.svg', type: 'image/svg+xml' },
+      { url: '/favicons/favicon.ico' },
+    ],
+    apple: { url: '/favicons/apple-touch-icon.png', sizes: '180x180' },
+  },
+  manifest: '/favicons/site.webmanifest',
 }
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,14 @@ export default function Page() {
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file. Hello!1
           </h1>
+          <div>
+            <Image
+              src="/images/granny.png"
+              alt="Granny"
+              width={200}
+              height={200}
+            />
+          </div>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{' '}
             <a
